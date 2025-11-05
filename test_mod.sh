c5e7cb390468f73af8012fe887c68ea4f3e95185
0xViva <0xViva@proton.me>
2025-11-05 15:31:43 +0100

    add test script

---
+++ test_mod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set the source and destination paths
+SOURCE="build/libs/*.jar"
+DEST="$HOME/.minecraft/mods"
+
+# Copy the jar file(s) to the mods folder
+echo "Copying jar files from $SOURCE to $DEST"
+cp -f $SOURCE $DEST
+
+echo "Done!"
