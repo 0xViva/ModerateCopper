af5a02f34732a59200806d76dc28053e022c2bba
0xViva <0xViva@proton.me>
2025-11-06 16:01:54 +0100

    add game rundata and gametests to all builds + move mod entry point

--- test_mod.sh
+++ test_mod.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+echo "cleanup:"
+./gradlew clean
+echo "rundata:"
+./gradlew runData
+echo "build:"
+./gradlew build
+echo "runGametest:"
+./gradlew runGametest
 # Set the source and destination paths
 SOURCE="build/libs/*.jar"
 DEST="$HOME/.minecraft/mods"
