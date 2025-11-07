dcb8d30d866a15678095aae24ab3f1222285b942
0xViva <0xViva@proton.me>
2025-11-07 15:36:03 +0100

    make mod for minecraft version 1.21.1

--- src/main/java/dev/oxviva/moderatecopper/datagen/ModDataGenerator.java
+++ src/main/java/dev/oxviva/moderatecopper/datagen/ModDataGenerator.java
@@ -3,6 +3,7 @@ package dev.oxviva.moderatecopper.datagen;
 import dev.oxviva.moderatecopper.ModerateCopper;
 import java.util.List;
 import java.util.Set;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,17 +12,21 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = ModerateCopper.MODID)
 public class ModDataGenerator {
-  @SubscribeEvent
-  public static void onGatherData(GatherDataEvent.Client event) {
 
-    event.createProvider(
-        (output, registries) ->
-            new LootTableProvider(
-                output,
-                Set.of(),
-                List.of(
-                    new LootTableProvider.SubProviderEntry(
-                        ModifiedCopperLootTable::new, LootContextParamSets.BLOCK)),
-                registries));
+  @SubscribeEvent
+  public static void onGatherData(GatherDataEvent event) {
+    event
+        .getGenerator()
+        .addProvider(
+            event.includeServer(),
+            (DataProvider.Factory<LootTableProvider>)
+                (output) ->
+                    new LootTableProvider(
+                        output,
+                        Set.of(), // No validation tables
+                        List.of(
+                            new LootTableProvider.SubProviderEntry(
+                                ModifiedCopperLootTable::new, LootContextParamSets.BLOCK)),
+                        event.getLookupProvider()));
   }
 }
